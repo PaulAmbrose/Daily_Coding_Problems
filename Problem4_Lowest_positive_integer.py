58df8eed02fc5a0b34df53e24798570dd435a236
PaulAmbrose <pablo_rose34@hotmail.com>
2018-12-21 16:56:37 +0000

    Add files via upload

--- Problem4_Lowest_positive_integer.py
+++ Problem4_Lowest_positive_integer.py
@@ -6,37 +6,16 @@
 #For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should
 #give 3.
 
-#Plan
-#1)For loop to look at each integer
-#2)If its zero or negative skip over
-#3)If its postive run function to check if its the lowest so far
-#4)Decision:
-     Separate decision function
-       
-               
-def main():
-    
-    input = [3, 4, -1, 1, 2]
-    lowest = 0
+input = [1, 2, 0]
 
-    def lowest_check(number, lowest):
-      if number <= 0:
-        return lowest
-      else:
-        number <= lowest
-        lowest = number
-        return lowest
-
-    def decision_check(lowest_number):
-     
-        
-    
-    for number in input:
-      new_lowest = lowest_check(number, lowest)
-      lowest = new_lowest
-      
-    answer = decision_check(lowest)
-    print(answer)
-
-if __name__ == "__main__":
-    main()
+input.sort()
+for number in input:
+  if number < 0:
+    input.remove(number)
+counter = 1
+while counter < (len(input)+1):
+  if counter in input:
+    counter = counter +1
+  else:
+    pass
+print(counter)
